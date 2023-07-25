6011a50ae86055a0a1eb9e8b5d24e5c3cdf2cc0c
mahmud-devs <mahmudulislam0999@gmail.com>
2023-07-26 04:21:35 +0600

    service part

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -10,6 +10,8 @@ tailwind.config = {
           header2c : '#005ec7',
           navcolor: '' ,
           perac: '#aaaaaa',
+          ggray: '#dce0e3d9',
+          pera:'rgba(0,0,0,.6)'
         },
         backgroundImage: {
             
