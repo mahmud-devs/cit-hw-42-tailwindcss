0ee8ec62cf0565cb213480062d0d65c603902760
mahmud-devs <mahmudulislam0999@gmail.com>
2023-07-26 01:36:14 +0600

    nav and service complete

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        maxWidth: {
+            container: '1260px',
+        },
+        colors: {
+          clifford: '#da373d',
+          headerc: '#a200ff',
+          header2c : '#005ec7',
+          navcolor: '' ,
+        },
+        backgroundImage: {
+            
+        },
+      },
+    },
+}
