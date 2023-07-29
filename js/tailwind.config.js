55815ff98b88f62d0af739b39c26aa279716f20f
mahmud-devs <mahmudulislam0999@gmail.com>
2023-07-30 00:45:54 +0600

    banner part

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,9 +1,9 @@
 tailwind.config = {
     theme: {
       extend: {
-        maxWidth: {
-            container: '1260px',
-        },
+        // maxWidth: {
+        //     container: '1260px',
+        // },
         colors: {
           clifford: '#da373d',
           headerc: '#a200ff',
